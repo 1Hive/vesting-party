0019213f421abf221f0ee419e36640aa539cdd8d
Gabi <gabrielpk.18@gmail.com>
2021-02-07 20:11:07 -0300

    UI: fetch airtable data

--- packages/react-app/src/utils/airtable.js
+++ packages/react-app/src/utils/airtable.js
@@ -31,3 +31,35 @@ export function writeAirtableData(party, chainId, data) {
     )
   )
 }
+
+// TODO: Write
+// var Airtable = require("airtable");
+// var base = new Airtable({ apiKey: "keyaFQcjixVE9ngtN" }).base(
+//   "appoEtN1RpO4eniQJ"
+// );
+// Airtable.configure({ apiKey: "keyaFQcjixVE9ngtN" });
+
+// base("Table 1")
+//   .select({
+//     // Selecting the first 3 records in Grid view:
+//     fields: ["id", "address", "earnings"],
+//     view: "Grid view",
+//   })
+//   .eachPage(
+//     function page(records, fetchNextPage) {
+//       // This function (page) will get called for each page of records.
+//       records.forEach(function (record) {
+//         console.log(record.get("id"));
+//       });
+//       // To fetch the next page of records, call fetchNextPage.
+//       // If there are more records, page will get called again.
+//       // If there are no more records, done will get called.
+//       fetchNextPage();
+//     },
+//     function done(err) {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//     }
+//   );
